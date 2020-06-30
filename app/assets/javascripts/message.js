a8a3c599e69d4daccc84a57ea8115be3d17459c8
Yuto Koshiyama <yu_tok54@icloud.com>
2020-06-30 17:47:03 +0900

    insert-ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,16 @@
 $(function () {
-  console.log("jquery導入成功");
+
+  $('.main-bar__message-form').on('submit', function (e) {
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
 });
