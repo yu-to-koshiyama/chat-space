f7c989a6d3c512d060002801e65fe61a643f756a
Yuto Koshiyama <yu_tok54@icloud.com>
2020-07-01 12:46:57 +0900

    edit-message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,46 @@
 $(function () {
-
-  $('.main-bar__message-form').on('submit', function (e) {
+  function buildHTML(message) {
+    if (message.image) {
+      let html = `<div class="message-box">
+                              <div class="message-info">
+                                <div class="message-info__user">
+                                  ${message.user_name}
+                                </div>
+                                <div class="message-info__date">
+                                  ${message.created_at}
+                                </div>
+                              </div>
+                          <div class="message-body">
+                              <p class="message__body">
+                                ${message.body}
+                              </p>
+                              <img class="message__image" src="${message.image}">
+                          </div>
+                  </div>`
+      return html;
+    } else {
+      let html = `<div class="message-box">
+                    <div class="message-info">
+                      <div class="message-info__user">
+                        ${message.user_name}
+                      </div>
+                      <div class="message-info__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message-body">
+                      <p class="message__body">
+                        ${message.body}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+  $('.Form').on('submit', function (e) {
     e.preventDefault()
     let formData = new FormData(this);
-    let url = $(this).attr('action')
+    let url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -12,5 +49,8 @@ $(function () {
       processData: false,
       contentType: false
     })
+      .done(function (data) {
+        let html = buildHTML(data);
+      })
   });
 });
